4d499bab7727552f1a25f179426459a95713b5e2
Alex <alexpit63@gmail.com>
2018-07-29 18:49:27 +0300

    one more line has been added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World again");
         System.out.println("Line");
+        System.out.println("And one more time");
     }
 }
