f814a688288b3d3b144fe7a23405b5d3e2c1ddee
Alex <alexpit63@gmail.com>
2018-07-29 18:50:01 +0300

    Revert "Add line"
    This reverts commit cf11988

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World again");
         System.out.println("Line");
-        System.out.println("And one more time");
     }
 }
